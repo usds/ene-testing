dd31d0cf2d33ee4f2e4a8cfd0bbdbf7081d22c6d
Christopher R. Wren <Christopher.R.Wren@omb.eop.gov>
2024-01-18 10:41:52 -0400

    support parameters on test detail page
    accept locality and test params
    highlight differences between expected and actual
    flesh out usds endpoint to support household, category, threshold, and chip fields

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 from executor import Executor
 
@@ -8,12 +8,22 @@ app = Flask(__name__)
 
 @app.route('/test', methods=['GET'])
 def test():
-    locality = "localities/config_nj.yml"
+    locality = "localities/config_aa.yml"
     template = "test_templates/00001.yml"
+    if request.args.get('locality') is not None:
+        locality = "localities/config_" + request.args.get('locality').lower() + ".yml"
+    if request.args.get('test') is not None:
+        template = "test_templates/" + request.args.get('test') + ".yml"
+
     executor = Executor(locality)
     artifacts = executor.exec(template)
+    actual = artifacts["actual"]
+    expected = {}
+    for applicant in artifacts["expected"]:
+         expected[applicant['person_id']] = applicant['is_eligible']
     return render_template('results.html',
-                            applicants = artifacts["actual"])
+                            actual = actual,
+                            expected = expected)
 
 @app.route('/', methods=['GET'])
 def root():
